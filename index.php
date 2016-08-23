dd716976a0d0f4924e7e696ba5fa2a2d0673281f
God Mod <godmod23@web.de>
2016-08-23 18:35:11 +0200

    Uploaded Mirror Files

---
+++ index.php
@@ -0,0 +1,382 @@
+<?php
+ini_set('display_error', 1);
+$arrMirrorlist = array(
+	'mirrordl1.eqdkp-plus.eu',
+);
+
+
+define("EQDKP_INC", true);
+if(!file_exists('mirror_cache.php')) die("No mirror file available.");
+include_once('mirror_cache.php');
+
+$strAction = "list";
+$strParam = false;
+
+if(isset($_GET['c'])){
+	$strParam = intval($_GET['c']);
+	$strAction = 'category';
+}
+
+
+function showCategories(){
+	$arrCategories = array(
+		//Core
+		'0' => array(
+			'name' => 'EQdkp Plus Core',
+			'icon' => 'fa-cog',
+			'subtitle' => 'Download the latest version of EQdkp Plus.',
+		),
+		'7' => array(
+			'name' => 'Games',
+			'icon' => 'fa-gamepad',
+		),
+		'10' => array(
+			'name' => 'Hooks',
+			'icon' => 'fa-code',
+		),
+		'11' => array(
+			'name' => 'Languages',
+			'icon' => 'fa-globe',
+		),
+		'1' => array(
+			'name' => 'Plugins',
+			'icon' => 'fa-cogs',
+		),
+		'3' => array(
+			'name' => 'Portal Modules',
+			'icon' => 'fa-columns',
+		),
+		'2' => array(
+			'name' => 'Templates',
+			'icon' => 'fa-tint',
+		),
+		'12' => array(
+			'name' => 'Tools',
+			'icon' => 'fa-wrench',
+		),
+	);
+	$out = "";
+	foreach($arrCategories as $intKey => $val){
+		$out .= '
+			<div class="extCategoryContainer">
+		<div>
+			<div class="grid1">
+				<a href="?c='.$intKey.'" style="color:#000;"><i class="fa '.$val['icon'].'" ></i></a>
+			</div>
+			
+			<div class="grid7">
+				<h2 style="font-size: 17px;">
+					<a href="?c='.$intKey.'" style="display:block;">'.$val['name'].'</a>
+				</h2>
+				'.((isset($val['subtitle'])) ? $val['subtitle'] : '').'
+			</div>
+
+		</div>
+		<div class="clear"></div>
+	</div>
+		';
+	}
+	
+	return $out;
+}
+
+function showCategory($intCategoryID){
+	global $arrFiles, $arrCoreVersions;
+	
+	$out = '<a href="index.php"><i class="fa fa-chevron-left fa-lg" style="font-size: 20px;"></i> Back to Categories</a><br /><br />';
+
+	foreach($arrFiles as $key => $val){
+		$strDownloadLink = ($val['sf_filename'] != "") ? $val['sf_filename'] : 'http://'.get_random_mirror().'/'.$val['filename'];
+
+		if($intCategoryID == 0 && $key === 'extension_pk'){
+
+			$out .= '<div class="extCategoryContainer">
+		<div>
+			<div class="grid1">
+				<a href="'.$strDownloadLink.'">
+				<i class="fa fa-cog"></i>
+				</a>
+			</div>
+			
+			<div class="grid7" style="max-width: 800px;">
+				<h2 style="font-size: 17px;">
+					<a href="'.$strDownloadLink.'" style="display:block;">EQdkp Plus Core, '.resolveCoreVersion($val['version']).'</a>
+				</h2>
+				Released '.date('d-m-Y', $val['releasedate']).'<br />
+				
+				<a href="'.$strDownloadLink.'" style="display:block;"><i class="fa fa-lg fa-download"></i> Download</a><br /><br />
+				
+				The EQdkp Plus Core Version comes without plugins, portal modules and templates. You can download all extensions at the Extension Management Page after you have installed EQdkp Plus or from our Download-Section.
+			</div>
+
+		</div>
+		<div class="clear"></div>
+	</div>';
+			break;
+			
+		}
+		
+		if($intCategoryID == $val['category']){
+			if($key === 'extension_pk') continue;
+			
+			$out .= '<div class="extCategoryContainer">
+		<div>
+			<div class="grid1">
+				<a href="'.$strDownloadLink.'">
+				<i class="fa fa-download"></i>
+				</a>
+			</div>
+			
+			<div class="grid7" style="max-width: 800px;">
+				<h2 style="font-size: 17px;">
+					<a href="'.$strDownloadLink.'" style="display:block;">'.$val['name'].', '.$val['version'].'</a>
+				</h2>
+				Released '.date('d-m-Y', $val['tstamp']).'<br />
+				For EQdkp Plus '.resolveCoreVersion($val['dep_coreversion']).'<br /><br />
+				'.$val['description'].'<br />
+				<a href="'.$strDownloadLink.'" style="display:block;"><i class="fa fa-lg fa-download"></i> Download</a><br />
+				
+
+				
+			</div>
+
+		</div>
+		<div class="clear"></div>
+	</div>';
+			
+		}
+		
+		
+	}
+	
+	echo $out;
+}
+
+function resolveCoreVersion($strVersion){
+	global $arrCoreVersions;
+	return (isset($arrCoreVersions[$strVersion]) ? $arrCoreVersions[$strVersion] : $strVersion);
+}
+
+function get_random_mirror(){
+	global $arrMirrorlist;
+	
+	$randomKey = array_rand($arrMirrorlist);
+	return $arrMirrorlist[$randomKey];
+}
+
+function execute(){
+	global $strAction, $strParam;
+	
+	if($strAction == 'list'){
+		echo showCategories();
+	} elseif($strAction == 'category'){
+		showCategory($strParam);
+	}
+}
+
+?>
+<!DOCTYPE html>
+						<html>
+						<head>
+							<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+							<title>EQdkp Plus - Repository Mirror 1</title>
+							<link rel='stylesheet' href='src/fontawesome/font-awesome.min.css' type='text/css' media='screen' />
+							<style type="text/css">
+							/* body */
+							html {
+								height: 100%;
+							}
+							
+							body {
+								background: #0e0e0e;
+								font-size: 14px;
+								font-family: Tahoma,Arial,Verdana,sans-serif;
+								color: #000;
+								padding:0;
+							  	margin:0;
+								line-height: 20px;
+							}
+								
+							
+							.wrapper{
+								background: #2e78b0; /* Old browsers */
+								background: -moz-linear-gradient(top,  #2e78b0 0%, #193759 100%); /* FF3.6+ */
+								background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#2e78b0), color-stop(100%,#193759)); /* Chrome,Safari4+ */
+								background: -webkit-linear-gradient(top,  #2e78b0 0%,#193759 100%); /* Chrome10+,Safari5.1+ */
+								background: -o-linear-gradient(top,  #2e78b0 0%,#193759 100%); /* Opera 11.10+ */
+								background: -ms-linear-gradient(top,  #2e78b0 0%,#193759 100%); /* IE10+ */
+								background: linear-gradient(to bottom,  #2e78b0 0%,#193759 100%); /* W3C */
+								filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#2e78b0', endColorstr='#193759',GradientType=0 ); /* IE6-9 */
+								font-size: 14px;
+								font-family: Tahoma,Arial,Verdana,sans-serif;
+								color: #000000;
+								padding:0;
+							  	margin:0;
+								line-height: 20px;
+							}							
+							
+							.header {
+								padding-top: 10px;
+								font-size: 45px;
+								font-weight: bold;
+								text-shadow: 1px 1px 2px #fff;
+								filter: dropshadow(color=#fff, offx=1, offy=1);
+								border: none;
+								color:  #fff;
+								text-align:center;
+								vertical-align: middle;
+								font-family: 'Trebuchet MS',Arial,sans-serif;
+								
+								background: url(src/background-head.svg) no-repeat scroll center top transparent;
+								background-size: 100%;
+							}
+							
+							.header img {
+								height: 150px;
+								vertical-align: middle;
+							}
+							
+							.footer {
+								margin-top: 10px;
+								color: #fff;
+								text-align: center;
+								background-color: #0e0e0e;
+								padding: 10px 40px 10px;
+							}
+							
+							.footer a, .footer a:link, .footer a:visited {
+								color: #fff;
+								text-decoration: none;
+							}
+							
+							.footer a:hover {
+								text-decoration: underline;
+							}
+							
+							.innerWrapper {
+								background-color: #f8f8f8;
+							}
+									
+							h1, h2, h3 {
+								font-family: 'Trebuchet MS',Arial,sans-serif;
+							    font-weight: bold;
+							    margin-bottom: 10px;
+							    padding-bottom: 5px;
+								border-bottom: 1px solid #CCCCCC;
+								margin-top: 5px;
+							}
+							
+							h1 {
+							    font-size: 20px;
+							}
+							
+							h2 {
+								font-size: 18px;
+							}
+							
+							h3 {
+								font-size: 14px;
+								border-bottom: none;
+								margin-bottom: 5px;
+							}
+									
+							/* Links */
+							a,a:link,a:active,a:visited {
+								color: #4E7FA8;
+								text-decoration: none;
+							}
+							
+							a:hover {
+								color: #000;
+								text-decoration: none;
+							}
+							
+							.content {
+								width: 960px;
+								padding: 5px;
+								margin: auto;
+							}
+							
+							.extCategoryContainer {
+								padding: 15px;
+								border: 1px solid #DDD;
+								background-color: #fff;
+								margin-bottom: 10px;
+							}
+							
+							.extCategoryContainer .grid1 i {
+								font-size: 60px;
+							}
+							
+							*[class*="grid"] {
+    float: left;
+    margin-left: 10px;
+    margin-right: 10px;
+    display: inline;
+}
+
+.grid1 {
+    width: 60px;
+}
+
+.clear {
+	clear: both;
+}
+
+.header h1 {
+    padding-left: 40px;
+    padding-top: 30px;
+    font-size: 50px;
+    font-weight: bold;
+    text-shadow: 1px 1px 2px #fff;
+    filter: dropshadow(color=#fff, offx=1, offy=1);
+    border: none;
+	line-height: 55px;
+}
+
+.header img {
+	float: left;
+}
+
+.headerInner{
+	width: 960px;
+	margin: auto;
+}
+						</style>
+						</head>
+
+						<body>
+									
+						<div class="wrapper">
+							<div class="header">
+								<div class="headerInner">
+									<a href="index.php"><img src="src/logo.svg" alt="EQdkp Plus" class="absmiddle" style="height: 130px;"/></a>
+									<h1>EQdkp Plus Repository Mirror 1</h1>
+									<div class="clear"></div>
+								</div>
+							</div>
+		
+							<div class="innerWrapper">
+								<div class="content">
+									<?php
+										execute();
+									?>
+								
+								</div>
+
+							</div>	
+							
+					</div>	
+					<div class="footer">
+							<a href="https://www.facebook.com/EQdkpPlus"><i class="fa fa-lg fa-facebook"></i> Facebook</a><br />
+							<a href="https://twitter.com/#!/EQdkpPlus"><i class="fa fa-lg fa-twitter"></i> Twitter</a><br /><br />
+						
+					
+					
+						<a href="http://eqdkp-plus.eu" target="_new">EQDKP Plus</a> &copy; 2003 - 2016 by EQDKP Plus Developer Team | Mirror last update: <?php if (isset($intLastUpdate)) echo date("d-m-Y H:i", $intLastUpdate); ?>
+							
+							
+					</div>	
+					</body>
+					</html>
---
+++ mirror_cache.php
@@ -0,0 +1,144 @@
+<?php
+if(!defined('EQDKP_INC')) {
+	header('HTTP/1.0 404 Not Found');exit;
+}
+
+$arrFiles = array (
+  0 => 
+  array (
+    'id' => '47',
+    'tstamp' => '1388743160',
+    'user_id' => '5',
+    'category' => '7',
+    'folderid' => 'aoc',
+    'name' => 'Age of Conan',
+    'description' => '',
+    'version' => '2.1',
+    'release_level' => 'stable',
+    'changelog' => '',
+    'changelog_url' => NULL,
+    'dep_coreversion' => '1.0.7',
+    'dep_coreversion_to' => '1.99',
+    'downloads' => '2166',
+    'filename' => 'games/aoc_1.zip',
+    'sf_filename' => '',
+    'status' => '2',
+    'filehash' => '793bd58841674d8ce0a0217ac17662284a0d12e4',
+    'rating' => '0',
+    'rating_sum' => '0',
+    'rating_count' => '0',
+    'signature' => 'IP9+d8ppHZMC9qGNyKPfSN29irJXq9ou3jQ3qXzcJSkjIu3llVJdP9pV5DkI+l0G/TK2UYT9/wpfiBfd7EemdONC6sWWVAahYUdzC5eUNrq9kqjpg/lX1NDgYDPC+IRzCNL6k82YCC0/pkDa5tX3Nj6UyFTzFfd5UMRcz6owSL+hWj91Io0raejwYNEWQHOBXtFz/MKldDq6orPl/j0Vn6LjmB9GkOjrQhiJjby4o903KMpvicIU6v/yXxzHygSL05owMPDsmzLI5ID8Ln+6Y/dRQ32MoY2pB5vRfi4/avRA1dLBy1BbDwop+pUGAQLFDKDyPrJsyBLbCeglh9Ipew==',
+    'tags' => NULL,
+    'demo_url' => '',
+    'wiki_url' => '',
+    'isupdate' => '0',
+    'screenshot' => 'aoc_1.png',
+    'dep_phpversion' => '5.2',
+    'license' => NULL,
+    'btproductid' => '0',
+    'bugtracker_url' => NULL,
+    'add_users' => NULL,
+    'username' => 'Sionaa',
+    'feed' => '1',
+  ),
+  'extension_pk' => 
+  array (
+    'plugin_id' => 1,
+    'version' => '2.2.12.0',
+    'level' => 'stable',
+    'name' => 'EQdkp Plus Core',
+    'plugin' => 'pluskernel',
+    'changelog' => '2.2.12
+- Added missing contant
+- Fixed php error handling
+
+2.2.11
+- Fixed Installer
+
+2.2.10
+- Fixed some small bugs',
+    'releasedate' => 1471737380,
+    'category' => 1,
+    'author' => 'EQdkp Plus Dev Team',
+    'dep_coreversion' => '2.2.11.0',
+    'shortdesc' => 'EQdkp Plus Core Version',
+    'downloads' => 0,
+    'rating' => 0,
+    'views' => 0,
+    'dep_php' => '5.5',
+    'bugtracker_url' => '{',
+    'filename' => 'core/2.2.12_1/eqdkp-plus_2.2.12_core.zip',
+  ),
+);
+
+$arrCoreVersions = array (
+  '2.1.1.0' => '2.1.1',
+  '2.3.0.20' => '2.3.0',
+  '2.2.0.3' => '2.2.0 RC1',
+  '2.1.999' => '2.1.999',
+  '2.0.5.0' => '2.0.5',
+  '1.0.18' => '1.0.1',
+  '1.0.19' => '1.0.2',
+  '1.0.20' => '1.0.3',
+  '1.99' => '1.99',
+  '2.0.0.25' => '2.0.0',
+  '1.0.21' => '1.0.4',
+  '1.0.22' => '1.0.5',
+  '1.0.23' => '1.0.6',
+  '1.0.24' => '1.0.7',
+  '1.0.25' => '1.0.8',
+  '1.0.26' => '1.0.9',
+  '1.0.27' => '1.0.10',
+  '1.0.28' => '1.0.11',
+  '1.0.29' => '1.0.12',
+  '2.0.999' => '2.0.99',
+  '2.1.0.9' => '2.1.0',
+  '2.0.4.0' => '2.0.4',
+  '2.0.3.0' => '2.0.3',
+  '2.0.2.0' => '2.0.2',
+  '2.0.1.0' => '2.0.1',
+  '2.1.2.0' => '2.1.2',
+  '2.1.3.0' => '2.1.3',
+  '2.1.4.0' => '2.1.4',
+  '2.1.5.0' => '2.1.5',
+  '2.2.8.0' => '2.2.8',
+  '2.2.0.4' => '2.2.0',
+  '2.2.1.0' => '2.2.1',
+  '2.2.2.0' => '2.2.2',
+  '2.2.3.0' => '2.2.3',
+  '2.2.4.0' => '2.2.4',
+  '2.2.999' => '2.2.999',
+  '2.2.5.0' => '2.2.5',
+  '2.2.6.0' => '2.2.6',
+  '2.2.7.0' => '2.2.7',
+  '2.2.9.0' => '2.2.9',
+  '2.2.10.0' => '2.2.10',
+  '2.2.11.0' => '2.2.11',
+  '2.2.12.0' => '2.2.12',
+  '2.2.13.0' => '2.2.13',
+);
+
+$arrCoreUpdates = array (
+  27 => 
+  array (
+    'plugin_id' => 103,
+    'version' => '1.0.28',
+    'level' => 'stable',
+    'name' => 'Live Update 1.0.10 to 1.0.11',
+    'plugin' => 'pluskernel',
+    'changelog' => '',
+    'releasedate' => 1412876155,
+    'category' => 1,
+    'author' => 'EQdkp Plus Dev Team',
+    'dep_coreversion' => '1.0.27',
+    'shortdesc' => 'Live Update 1.0.10 to 1.0.11',
+    'downloads' => 0,
+    'rating' => 0,
+    'views' => 0,
+    'dep_php' => '5.3',
+    'bugtracker_url' => NULL,
+    'filename' => 'core/1.0.11_1/Update_Packages/eqdkp-plus_1.0.10_to_1.0.11_update.zip',
+  ),
+);
+
+$intLastUpdate = 1471957203;
